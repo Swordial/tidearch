9573cf5b3a39800016dbd01225a8fcbefcc9fd7d
Swordial <oceanrelocator202@gmail.com>
2026-03-10 20:24:52 -0700

    Create cbestfetch function for bestfetch alias
    Add function cbestfetch to wrap bestfetch command

---
+++ .config/fish/functions/cbestfetch.fish
@@ -0,0 +1,3 @@
+function cbestfetch --wraps='ox ~/.config/bestfetch/config.yaml' --description 'alias cbestfetch=ox ~/.config/bestfetch/config.yaml'
+    ox ~/.config/bestfetch/config.yaml $argv
+end
